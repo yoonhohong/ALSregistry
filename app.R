88bb83d6f9fd2d4c09102db54a2d90b1e8ad3b91
yoonhohong <nrhong@gmail.com>
2020-01-15 10:30:07 +0900

    textOutput biobank start

--- app.R
+++ app.R
@@ -15,10 +15,10 @@ ui = fluidPage(
                                              "FAS", "FLS", "SBMA", "BFA", 
                                              "MMN", "Others"), multiple = TRUE, 
                         selected = "ALS"), 
-            dateInput("date_dx", "Diagnosis (since) # 5 years ago (default)", 
+            dateInput("date_dx", "Diagnosis after", 
                       value = Sys.Date() - 365*5, format = "yyyy-mm-dd"), 
-            sliderInput("fu_dur", "FU duration (>, months)", min = 0, max = 24, value = 12), 
-            dateInput("date_fu_last", "Latest FU (since) # recent 4 year (default)", 
+            numericInput("fu_dur", "FU duration (months) longer than", value = 12), 
+            dateInput("date_fu_latest", "Latest FU after", 
                       value = Sys.Date() - 365*4, format = "yyyy-mm-dd")
         ),
         column(5, 
@@ -30,18 +30,21 @@ ui = fluidPage(
         column(4, 
                h4("DX & FU Hx"), 
                hr(),
-               h6(textOutput("date_dx"), style="color:red"), 
-               h6(textOutput("onset2dx")),  
-               h6(textOutput("onset_region")), 
-               h6(textOutput("fu_duration")), 
-               h6(textOutput("date_fu_last"), style="color:red"),
+               h5(textOutput("date_dx"), style="color:red"), 
+               h5(textOutput("onset2dx")),  
+               h5(textOutput("onset_region")), 
+               h5(textOutput("fu_duration")), 
+               h5(textOutput("date_fu_latest"), style="color:red"),
                hr(), 
-               h6(textOutput("gastrostomy")), 
-               h6(textOutput("NIV")),
-               h6(textOutput("tracheostomy")), 
-               h6(textOutput("death")), 
-               hr(), 
-               h6(textOutput("Biobank"))
+               h5(textOutput("gastrostomy")), 
+               h5(textOutput("NIV")),
+               h5(textOutput("tracheostomy")), 
+               hr(),
+               h5(textOutput("death"), style="color:red"), 
+               h5(textOutput("refer")), 
+               h5(textOutput("lost_fu")), 
+               hr(),
+               h5(textOutput("Biobank"))
                ), 
         column(4, 
                h4("ALSFRS revised (total score)"),
@@ -52,16 +55,15 @@ ui = fluidPage(
             )
     )
 
-
 server = function(input, output){
     
     selected_id1 = reactive({
         intersect(
             intersect(
                 intersect(subset(base, Dx %in% input$dx)$Study_ID, 
-                          subset(fu, Visit_interval * 4 > input$fu_dur)$Study_ID), 
+                          subset(fu, Visit_interval/4 > input$fu_dur)$Study_ID), 
                 subset(dx, Date_dx > input$date_dx)$Study_ID),
-            subset(fu, Date_visit > input$date_fu_last)$Study_ID)
+            subset(fu, Date_visit > input$date_fu_latest)$Study_ID)
     })
     
     output$pt_list_tbl = renderDT({
@@ -73,9 +75,10 @@ server = function(input, output){
         s = input$pt_list_tbl_rows_selected
         if (is.null(s)) {
             return(NULL)
-        }
-        temp[s,]$Study_ID
-    })
+        } else {
+            temp[s,]$Study_ID
+            }
+        })
     
     output$plot_alsfrs = renderPlot({
         temp = fu %>%
@@ -120,70 +123,141 @@ server = function(input, output){
         })
 
     output$date_dx = renderText({
-        temp = dx[dx$Study_ID %in% selected_id2(),]$Date_dx
-        paste("Dx: ", format(temp, "%Y-%m"))
+        if (is.null(selected_id2())) {
+            print("Dx: ")
+        } else {
+            temp = dx[dx$Study_ID %in% selected_id2(),]
+            paste("Dx: ", format(temp$Date_dx, "%Y.%m"))
+            }
         })
     
     output$onset2dx = renderText({
-        temp1 = dx[dx$Study_ID %in% selected_id2(),]
-        temp2 = round(difftime(temp1$Date_dx, temp1$Date_onset, units = "weeks"))
-        paste("Time from onset to Dx (weeks): ", temp2)
+        if (is.null(selected_id2())) {
+            print("Time from onset to Dx (months): ")
+        } else {
+            temp1 = dx[dx$Study_ID %in% selected_id2(),]
+            temp2 = round(difftime(temp1$Date_dx, temp1$Date_onset, units = "weeks"))
+            paste("Time from onset to Dx (months): ", round(temp2/4))
+            }
         })
     
     output$onset_region = renderText({
-        temp = dx[dx$Study_ID %in% selected_id2(),]$Onset_region
-        paste("Onset region: ", temp)
-    })
-    
-    output$fu_duration = renderText({
-        temp1 = fu[fu$Study_ID %in% selected_id2(),]
-        temp2 = round(difftime(last(temp1$Date_visit), first(temp1$Date_visit),
-                               units = "weeks"))
-        paste("FU duration (weeks): ", temp2)
+        if (is.null(selected_id2())) {
+            print("Onset region: ")
+        } else {
+            temp = dx[dx$Study_ID %in% selected_id2(),]$Onset_region
+            paste("Onset region: ", temp)
+        }
         })
     
-    output$date_fu_last = renderText({
-        temp1 = fu[fu$Study_ID %in% selected_id2(),]$Date_visit
-        paste("Last fu: ", last(temp1))
-    })
+    output$fu_duration = renderText({
+        if (is.null(selected_id2)) {
+            print("FU duration (months):")
+        } else {
+            temp1 = fu[fu$Study_ID %in% selected_id2(),]
+            temp2 = round(difftime(last(temp1$Date_visit), first(temp1$Date_visit),
+                                   units = "weeks"))
+            paste("FU duration (months): ", round(temp2/4))
+            }
+        })
+    
+    output$date_fu_latest = renderText({
+        if (is.null(selected_id2())) {
+            print("Latest FU: ")
+        } else {
+            temp = fu[fu$Study_ID %in% selected_id2(),]
+            paste("Latest FU: ", format(last(temp$Date_visit), "%Y.%m.%d"))
+            }
+        })
 
     output$gastrostomy = renderText({
-        temp = event %>% 
-            filter(Study_ID %in% selected_id2()) %>%
-            filter(Event == "Gastrostomy")
-        if (dim(temp)[1] == 0) {
-            paste("Gastrostomy: ", NA)
+        if (is.null(selected_id2())) {
+            print("Gastrostomy:")
         } else {
-            paste("Gastrostomy: ", format(temp$Date_event, "%Y-%m-%d"))
+            temp = event %>% 
+                filter(Study_ID %in% selected_id2()) %>%
+                filter(Event == "Gastrostomy")
+            if (dim(temp)[1] == 0) {
+                paste("Gastrostomy: ", NA)
+            } else {
+                paste("Gastrostomy: ", format(temp$Date_event, "%Y.%m.%d"))
+            }
         }
     })
     
     output$NIV = renderText({
-        temp = event %>% 
-            filter(Study_ID %in% selected_id2()) %>%
-            filter(Event == "NIV")
-        if (dim(temp)[1] == 0) {
-            paste("NIV: ", NA)
+        if (is.null(selected_id2())) {
+            print("NIV:")
         } else {
-            paste("NIV: ", format(temp$Date_event, "%Y-%m-%d"))
+            temp = event %>% 
+                filter(Study_ID %in% selected_id2()) %>%
+                filter(Event == "NIV")
+            if (dim(temp)[1] == 0) {
+                paste("NIV: ", NA)
+            } else {
+                paste("NIV: ", format(temp$Date_event, "%Y.%m.%d"))
+            }
         }
     })
     
     output$tracheostomy = renderText({
-        temp = event %>% 
-            filter(Study_ID %in% selected_id2()) %>%
-            filter(Event == "Tracheostomy")
-        if (dim(temp)[1] == 0) {
-            paste("Tracheostomy: ", NA)
+        if (is.null(selected_id2())) {
+            print("Tracheostomy:")
         } else {
-            paste("Tracheostomy: ", format(temp$Date_event, "%Y-%m-%d"))
+            temp = event %>% 
+                filter(Study_ID %in% selected_id2()) %>%
+                filter(Event == "Tracheostomy")
+            if (dim(temp)[1] == 0) {
+                paste("Tracheostomy: ", NA)
+            } else {
+                paste("Tracheostomy: ", format(temp$Date_event, "%Y.%m.%d"))
+            }
         }
     })
     
     output$death = renderText({
-        temp = event[event$Study_ID %in% selected_id2(),]$Date_event
-        temp = ifelse(is.null(temp), "NA", temp)
-        paste("Death: ", temp)
+        if (is.null(selected_id2())) {
+            print("Death:")
+        } else {
+            temp = close %>%
+                filter(Study_ID %in% selected_id2()) %>%
+                filter(Close_reason == "Death")
+            if (dim(temp)[1] == 0) {
+                paste("Death: ", NA)
+            } else {
+                paste("Death: ", format(temp$Date_close, "%Y.%m.%d"))
+            }
+        }
+    })
+    
+    output$lost_fu = renderText({
+        if (is.null(selected_id2())) {
+            print("Lost to f/u:")
+        } else {
+            temp = close %>%
+                filter(Study_ID %in% selected_id2()) %>%
+                filter(Close_reason == "Lost to f/u")
+            if (dim(temp)[1] == 0) {
+                paste("Lost to f/u: ", NA)
+            } else {
+                paste("Lost to f/u: ", format(temp$Date_close, "%Y.%m.%d"))
+            }
+        }
+    })
+    
+    output$refer = renderText({
+        if (is.null(selected_id2())) {
+            print("Refer:")
+        } else {
+            temp = close %>%
+                filter(Study_ID %in% selected_id2()) %>%
+                filter(Close_reason == "Refer")
+            if (dim(temp)[1] == 0) {
+                paste("Refer: ", NA)
+            } else {
+                paste("Refer: ", format(temp$Date_close, "%Y.%m.%d"))
+            }
+        }
     })
 }
 
--- global.R
+++ global.R
@@ -7,14 +7,14 @@ library(janitor)
 base = read_csv('ALS_Registry_Base.csv', 
                 locale = locale(encoding = "CP949"), 
                 col_types = cols_only(
-                  Study_ID = col_character(), 
+                  Study_ID = col_factor(), 
                   Hosp_ID = col_character(), 
                   Name = col_character()
                 )) %>% remove_empty("rows")
 
 dx = read_csv("ALS_Registry_Dx.csv", 
               col_types = cols_only(
-                Study_ID = col_character(), 
+                Study_ID = col_factor(), 
                 Sex = col_factor(), 
                 Date_birth = col_date(format = "%Y.%m.%d"),
                 Date_onset = col_date(format = "%Y.%m.%d"),
@@ -33,7 +33,7 @@ base = merge(base, dx, all.x = T, by = "Study_ID") %>%
 
 fu = read_csv('ALS_Registry_Followup.csv', 
               col_types = cols(
-                Study_ID = col_character(),
+                Study_ID = col_factor(),
                 Date_visit = col_date(format = "%Y.%m.%d"), 
                 Wt = col_double(),
                 FVC_percent = col_double(),
@@ -55,13 +55,48 @@ fu_fvc = fu %>%
 
 event = read_csv('ALS_Registry_Event.csv',
                  col_types = cols(
-                   Study_ID = col_character(),
+                   Study_ID = col_factor(),
                    Event = col_factor(), 
                    Date_event = col_date(format = "%Y.%m.%d")
                  ))
 event = event[complete.cases(event),]
 
-# close = read_csv('ALS_Registry_')
+close = read_csv('ALS_Registry_Close.csv', 
+                 col_types = cols(
+                   Study_ID = col_factor(),
+                   Close_reason = col_factor(),
+                   Date_close = col_date(format = "%Y.%m.%d")
+                 ))
+close = close[complete.cases(close),]
+
+bio1 = read_csv("ALS_Registry_Biobank1.csv", 
+                col_types = cols(
+                  Study_ID = col_factor(), 
+                  Hosp_ID = col_factor(),
+                  Provider_Ocode = col_factor()
+                ))
+bio1 = bio1[complete.cases(bio1),]
+
+bio2 = read_csv("ALS_Registry_Biobank2.csv", 
+                col_types = cols(
+                  Provider_Ocode = col_factor(), 
+                  Sample_Bcode = col_character()
+                ))
+bio2 = bio2[complete.cases(bio2),]
+
+bio = merge(bio1, bio2, all.y = T, by = "Provider_Ocode")
+# setdiff(bio2$Provider_Ocode, bio1$Provider_Ocode)
+
+ser = bio[grep("SER", bio$Sample_Bcode), ]
+buf = bio[grep("BUF", bio$Sample_Bcode), ]
+pla = bio[grep("PLA", bio$Sample_Bcode), ]
+csf = bio[grep("CSF", bio$Sample_Bcode), ]
+
+ser$visit_no = regmatches(ser$Sample_Bcode, regexpr("SER..", ser$Sample_Bcode))
+ser$visit_no = regmatches(ser$visit_no, regexpr("[0-9][0-9]", ser$visit_no))
+ser$visit_no = as.integer(ser$visit_no)
+
+
 
 
 
